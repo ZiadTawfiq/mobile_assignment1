416da8f680cd596b867c7c9e7d8a1d95a5980c6e
ziad <ziadtawfik2003@gmail.com>
2026-04-19 00:41:39 +0200

    fix requirements

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = "postgresql://postgres:EZdXAhWtBKoOXInHjzWNxrbXGvIdfzpk@postgres.railway.internal:5432/railway"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
-
-SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
